93f5fef1e7bfaad0b5e070ee2c596bda6feed5f0
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-03-09 00:03:13 +0700

    Change User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $administrator = User::create([
+            'name' => 'Administrator Sumbidik',
+            'email' => 'administrator@sumbidik.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $administrator->assignRole('Administrator');
+
+        $write = User::create([
+            'name' => 'Garllockhart',
+            'email' => 'garllockhart@sumbidik.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234567890'),
+            'remember_token' => Str::random(10),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $write->assignRole('Operator');
     }
 }
